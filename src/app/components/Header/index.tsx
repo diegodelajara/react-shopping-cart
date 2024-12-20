e16a0dd5b4e7b77bf9772961ecb2f634d0ffbf89
Diego de la jara <diegodelajara@MacBook-Pro-de-Diego.local>
2024-12-20 01:50:26 -0300

    style: :lipstick:

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -8,8 +8,10 @@ type HeaderProps = {
 
 export default function Header({ title, url }: HeaderProps) {
   return (
-    <div className="flex justify-between w-full">
-      <Link href={url}>{title}</Link>
+    <div className="flex justify-between w-full bg-slate-300 px-32 min-h-[64px] items-center">
+      <Link href={url} className="font-bold ">
+        {title}
+      </Link>
       <CartIcon />
     </div>
   );
