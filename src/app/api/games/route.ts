7457993c04dea216e64f7579932756a29b47b5d0
Diego de la Jara <d.delajaral@gmail.com>
2024-12-22 04:50:50 -0300

    feat: :sparkles: Add addToCart and remove methods

--- src/app/api/games/route.ts
+++ src/app/api/games/route.ts
@@ -1,28 +1,28 @@
 import { NextRequest } from 'next/server';
 
-type Game = {
-    id: string;
+export type Game = {
+    id?: string;
     genre: string;
     image: string;
-    name: string;
-    description: string;
+    title: string;
+    description?: string;
     price: number;
-    isNew: boolean;
+    isNew?: boolean;
   };
 
 const games: Game[] = [
-  { id: '1', genre: 'Battle Royale', image: '/assets/elden.png', name: 'Apex', description: 'Description 1', price: 10, isNew: true },
-  { id: '2', genre: 'Battle Royale', image: '/assets/hollow.png', name: 'Hallow Knight', description: 'Description 1', price: 10, isNew: true },
-  { id: '3', genre: 'RPG', image: '/assets/fornite.png', name: 'Fortnite', description: 'Description 2', price: 20, isNew: false },
-  { id: '4', genre: 'Action', image: '/assets/destiny.jpeg', name: 'Destiny 2', description: 'Description 3', price: 30, isNew: true },
-  { id: '5', genre: 'Battle Royal', image: '/assets/counter-strike.jpeg', name: 'Counter Strike 2', description: 'Description 3', price: 30, isNew: true },
-  { id: '6', genre: 'Action', image: '/assets/apex.jpeg', name: 'Apex', description: 'Description 3', price: 30, isNew: true },
-  { id: '7', genre: 'RPG', image: '/assets/ln.png', name: 'Little Nightmares', description: 'Description 3', price: 30, isNew: true },
-  { id: '8', genre: 'Action', image: '/assets/dead.jpeg', name: 'Dead by Daylight 3', description: 'Description 3', price: 30, isNew: true },
-  { id: '9', genre: 'Action', image: '/assets/gta.jpeg', name: 'Grand Theft Auto 5', description: 'Description 3', price: 30, isNew: true },
-  { id: '10', genre: 'Action', image: '/assets/helldivers.png', name: 'Hell Divers', description: 'Description 3', price: 30, isNew: true },
-  { id: '11', genre: 'Battle Royal', image: '/assets/fallout.jpeg', name: 'Fallout 4', description: 'Description 3', price: 30, isNew: true },
-  { id: '12', genre: 'Battle Royal', image: '/assets/codwz.jpeg', name: 'Call of Duty Warzone', description: 'Description 3', price: 30, isNew: true },
+  { id: '1', genre: 'Battle Royale', image: '/assets/elden.png', title: 'Apex', description: 'Description 1', price: 10, isNew: true },
+  { id: '2', genre: 'Battle Royale', image: '/assets/hollow.png', title: 'Hallow Knight', description: 'Description 1', price: 10, isNew: true },
+  { id: '3', genre: 'RPG', image: '/assets/fornite.png', title: 'Fortnite', description: 'Description 2', price: 20, isNew: false },
+  { id: '4', genre: 'Action', image: '/assets/destiny.jpeg', title: 'Destiny 2', description: 'Description 3', price: 30, isNew: true },
+  { id: '5', genre: 'Battle Royal', image: '/assets/counter-strike.jpeg', title: 'Counter Strike 2', description: 'Description 3', price: 30, isNew: true },
+  { id: '6', genre: 'Action', image: '/assets/apex.jpeg', title: 'Apex', description: 'Description 3', price: 30, isNew: true },
+  { id: '7', genre: 'RPG', image: '/assets/ln.png', title: 'Little Nightmares', description: 'Description 3', price: 30, isNew: true },
+  { id: '8', genre: 'Action', image: '/assets/dead.jpeg', title: 'Dead by Daylight 3', description: 'Description 3', price: 30, isNew: true },
+  { id: '9', genre: 'Action', image: '/assets/gta.jpeg', title: 'Grand Theft Auto 5', description: 'Description 3', price: 30, isNew: true },
+  { id: '10', genre: 'Action', image: '/assets/helldivers.png', title: 'Hell Divers', description: 'Description 3', price: 30, isNew: true },
+  { id: '11', genre: 'Battle Royal', image: '/assets/fallout.jpeg', title: 'Fallout 4', description: 'Description 3', price: 30, isNew: true },
+  { id: '12', genre: 'Battle Royal', image: '/assets/codwz.jpeg', title: 'Call of Duty Warzone', description: 'Description 3', price: 30, isNew: true },
 ];
 
 export async function GET(req: NextRequest) {
--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,14 +1,12 @@
 "use client";
+import { Game } from "@/app/api/games/route";
+import { useCart } from "@/Context.games";
 import React from "react";
 
-type ItemProps = {
-  name: string;
-  genre: string;
-  price: number;
-  image: string;
-};
+export default function Item({ id, title, genre, price, image }: Game) {
+  const { addToCart, isGameInCart, removeFromCart } = useCart();
+  const handleAddToCart = (game: Game) => addToCart(game);
 
-export default function Item({ name, genre, price, image }: ItemProps) {
   return (
     <div className="flex flex-col border rounded-[theme(spacing.4)] overflow-hidden p-6 w-[327px] gap-4 md:w-[auto] lg:w-[327px]">
       <div
@@ -20,13 +18,27 @@ export default function Item({ name, genre, price, image }: ItemProps) {
           {genre}
         </p>
         <div className="flex justify-between items-center">
-          <span className="text-xl font-bold mb-2 text-customGray">{name}</span>
+          <span className="text-xl font-bold mb-2 text-customGray">
+            {title}
+          </span>
           <span className="text-gray-900 font-semibold">${price}</span>
         </div>
       </div>
-      <button className=" font-bold text-customGray uppercase flex w-full justify-center border rounded-[theme(spacing.2)] p-[20px]">
-        add to cart
-      </button>
+      {isGameInCart(id!) ? (
+        <button
+          onClick={() => removeFromCart(id!)}
+          className=" font-bold text-customGray uppercase flex w-full justify-center border rounded-[theme(spacing.2)] p-[20px] hover:bg-slate-50 "
+        >
+          remove
+        </button>
+      ) : (
+        <button
+          onClick={() => handleAddToCart({ id, title, genre, price, image })}
+          className=" font-bold text-customGray uppercase flex w-full justify-center border rounded-[theme(spacing.2)] p-[20px] hover:bg-slate-50 "
+        >
+          add to cart
+        </button>
+      )}
     </div>
   );
 }
