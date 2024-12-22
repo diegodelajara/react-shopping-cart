6018da5ec5ef36319e9e625f1fbbe6908b504247
Diego de la Jara <d.delajaral@gmail.com>
2024-12-22 00:32:13 -0300

    feat: :sparkles: Add filter functions to display games by genre

--- src/components/Catalog/index.tsx
+++ src/components/Catalog/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 import Categories from "../Categories";
 import Item from "../Item";
+
 type CatalogProps = {
   title: string;
 };
+
 type Game = {
   id: string;
   genre: string;
@@ -13,16 +17,44 @@ type Game = {
   price: number;
   isNew: boolean;
 };
-export default async function Catalog({ title }: CatalogProps) {
-  const response = await fetch("http://localhost:3000/api/games");
-  const data = (await response.json()) as Game[];
+
+export default function Catalog({ title }: CatalogProps) {
+  const [games, setGames] = useState<Game[]>([]);
+  const [filteredGames, setFilteredGames] = useState<Game[]>([]);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre") || "All";
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      const response = await fetch("/api/games");
+      const data = (await response.json()) as Game[];
+      setGames(data);
+      setFilteredGames(data);
+    };
+
+    fetchGames();
+  }, []);
+
+  useEffect(() => {
+    if (genre === "All") {
+      setFilteredGames(games);
+    } else {
+      setFilteredGames(games.filter((game) => game.genre === genre));
+    }
+  }, [genre, games]);
+
   return (
     <div>
       <div className="flex justify-center">
         <div className="container mx-auto px-4 w-full">
           {title}
           <div className="flex w-full lg:justify-end">
-            <Categories />
+            <Categories
+              handleSelectCategory={(category) =>
+                router.push(`/?genre=${category}`)
+              }
+            />
           </div>
         </div>
       </div>
@@ -30,9 +62,9 @@ export default async function Catalog({ title }: CatalogProps) {
       <div className="container mx-auto px-2">
         <div className="flex justify-center">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pt-8 xs:p-6">
-            {data.map(({ name, genre, price, image }, key) => (
+            {filteredGames.map(({ id, name, genre, price, image }) => (
               <Item
-                key={key}
+                key={id}
                 name={name}
                 genre={genre}
                 price={price}
--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,13 +1,27 @@
+"use client";
 import React from "react";
 
-export default function Categories() {
+export default function Categories({
+  handleSelectCategory,
+}: {
+  handleSelectCategory: (category: string) => void;
+}) {
+  const categories = ["All", "Battle Royale", "RPG", "Action"];
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedCategory = e.target.value;
+    handleSelectCategory(selectedCategory);
+  };
+
   return (
     <div>
       <span>Genre |</span>
-      <select>
-        <option value="1">Option 1</option>
-        <option value="2">Option 2</option>
-        <option value="3">Option 3</option>
+      <select onChange={handleChange}>
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
       </select>
     </div>
   );
