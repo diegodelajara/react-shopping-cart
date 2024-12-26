3e5b73f7e3bf14479cbcc15060148920ae580d6c
Diego de la Jara <d.delajaral@gmail.com>
2024-12-26 04:13:58 -0300

    fix: :bug: front works

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
+module.exports = {
+  swcMinify: true,
+  webpack(config: import('webpack').Configuration) {
+    if (config.module && config.module.rules) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+    }
     return config;
-  }
+  },
 };
-
-export default nextConfig;
-
-
